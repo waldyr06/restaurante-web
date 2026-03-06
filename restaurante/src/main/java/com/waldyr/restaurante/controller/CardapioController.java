0e941397853417aa2b7c06826399d9b4c008c091
waldyr06 <waldyrfelype2012@gmail.com>
2026-03-06 02:16:35 -0300

    Adicionado tela nova de resumo, todos os botões funcionam corretamente

--- restaurante/src/main/java/com/waldyr/restaurante/controller/CardapioController.java
+++ restaurante/src/main/java/com/waldyr/restaurante/controller/CardapioController.java
@@ -5,13 +5,15 @@ import com.waldyr.restaurante.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 @Controller
 @RequestMapping("/cardapio")
+@SessionAttributes("carrinho")
 public class CardapioController {
 
     @Autowired
@@ -22,5 +24,45 @@ public class CardapioController {
         List<Produto> cardapio = service.listarApenasAtivos();
         model.addAttribute("produtos", cardapio);
         return "cliente/cardapio";
+    }
+
+    @ModelAttribute("carrinho")
+    public List<Produto> inicializarCarrinho(){
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/adicionar/{id}")
+    public String adicionarAoCarrinho(
+            @PathVariable Long id,
+            @ModelAttribute("carrinho") List<Produto> carrinho)
+    {
+        Produto pratoEscolhido = service.buscarPorId(id);
+        carrinho.add(pratoEscolhido);
+        return "redirect:/cardapio/comidas";
+    }
+
+    @GetMapping("/resumo")
+    public String resumoDoPedido(
+            @ModelAttribute("carrinho") List<Produto> carrinho,
+            Model model
+    ){
+        BigDecimal totalDaConta = BigDecimal.ZERO;
+
+        for (Produto prato : carrinho) {
+            totalDaConta = totalDaConta.add(prato.getPrice());
+        }
+
+        model.addAttribute("total", totalDaConta);
+
+        return "cliente/resumo";
+    }
+
+    @GetMapping("/finalizar")
+    public String finalizarPedido(
+            @ModelAttribute("carrinho") List<Produto> carrinho
+    ){
+        carrinho.clear();
+
+        return "redirect:/cardapio/comidas";
     }
 }
